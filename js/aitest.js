04ce5ea6fee95ca23a125ed504e9788502ec5433
Blaine Pace <blainepace@gmail.com>
2013-04-26 15:26:49 -0700

    Application spawns a colored grid of cubes

--- js/aitest.js
+++ js/aitest.js
@@ -1,24 +1,45 @@
-var WIDTH = 400, HEIGHT = 300;
+var WIDTH = 600, HEIGHT = 600;
 var VIEW_ANGLE = 45, ASPECT = WIDTH / HEIGHT, NEAR = 0.1, FAR = 10000;
-
 var $container = $('#container');
 
 var renderer = new THREE.WebGLRenderer();
 var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
 var scene = new THREE.Scene();
 
-camera.position.z = 300;
+camera.position.z = 200;
 renderer.setSize(WIDTH, HEIGHT);
+renderer.setClearColorHex(0x111111, 1.0);
 
 $container.append(renderer.domElement);
 
-// Add sphere to scene
-var radius = 50, segments = 16, rings = 16;
-var sphere_geom = new THREE.SphereGeometry(radius, segments, rings);
-var sphere_mat  = new THREE.MeshLambertMaterial({ color: 0xCC0000 });
-var sphere = new THREE.Mesh(sphere_geom, sphere_mat);
+// Application specific logic
+var CUBE_SIZE = 2;
+var CUBEGRID_SIZE = 40;
+var CUBEGRID_ITERS = 20;
+var CUBEGRID_STEPSIZE = CUBEGRID_SIZE / CUBEGRID_ITERS;
+var COLOR_SCALE = 0.05;
+var OFFSET_SCALE = 1.5;
+var cubes = new THREE.Object3D();
 
-scene.add(sphere);
+function create_cube(width, height, length) {
+  var cube_geom  = new THREE.CubeGeometry(width, height, length);
+  var cube_mat  = new THREE.MeshLambertMaterial({ color: 0xCC0000 });
+  var cube = new THREE.Mesh(cube_geom, cube_mat);
+  return cube;
+}
+
+for(var x = 0; x < CUBEGRID_ITERS; x++) {
+  for(var y = 0; y < CUBEGRID_ITERS; y++) {
+    var cube = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE);
+    cube.position.x = CUBEGRID_STEPSIZE * OFFSET_SCALE * x;
+    cube.position.y = CUBEGRID_STEPSIZE * OFFSET_SCALE * y;
+    cube.material.color.setRGB(0, COLOR_SCALE * x, COLOR_SCALE * y);
+    cubes.add(cube);
+  }
+}
+
+create_cube(80, 80, 80);
+scene.add(cubes);
 scene.add(camera);
 
 // Add light to the scene
@@ -28,4 +49,10 @@ pointlight.position.y = 50;
 pointlight.position.z = 130;
 scene.add(pointlight);
 
-renderer.render(scene, camera);
+function render() {
+  requestAnimationFrame(render);
+
+  renderer.render(scene, camera);
+}
+
+render();
