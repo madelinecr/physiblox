c94305af57df6a0d8d5b9f26eff836343fe5924a
Blaine Pace <blainepace@gmail.com>
2013-04-30 19:22:24 -0700

    Added physijs

--- js/aitest.js
+++ js/aitest.js
@@ -1,58 +1,70 @@
+'use strict';
+
+Physijs.scripts.worker = '/js/physijs_worker.js'
+Physijs.scripts.ammo = '/js/ammo.js'
+
 var WIDTH = 600, HEIGHT = 600;
 var VIEW_ANGLE = 45, ASPECT = WIDTH / HEIGHT, NEAR = 0.1, FAR = 10000;
 var $container = $('#container');
+var renderer, camera, scene;
+var cube;
 
-var renderer = new THREE.WebGLRenderer();
-var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-var scene = new THREE.Scene();
+function init() {
+  renderer = new THREE.WebGLRenderer();
+  camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+  scene = new Physijs.Scene();
 
-camera.position.z = 200;
-renderer.setSize(WIDTH, HEIGHT);
-renderer.setClearColorHex(0x111111, 1.0);
-
-$container.append(renderer.domElement);
-
-// Application specific logic
-var CUBE_SIZE = 2;
-var CUBEGRID_SIZE = 40;
-var CUBEGRID_ITERS = 20;
-var CUBEGRID_STEPSIZE = CUBEGRID_SIZE / CUBEGRID_ITERS;
-var COLOR_SCALE = 0.05;
-var OFFSET_SCALE = 1.5;
-var cubes = new THREE.Object3D();
-
-function create_cube(width, height, length) {
-  var cube_geom  = new THREE.CubeGeometry(width, height, length);
-  var cube_mat  = new THREE.MeshLambertMaterial({ color: 0xCC0000 });
-  var cube = new THREE.Mesh(cube_geom, cube_mat);
-  return cube;
+  camera.position.set( 200, 0, 0 );
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+  renderer.setSize(WIDTH, HEIGHT);
+  renderer.setClearColorHex(0x111111, 1.0);
+  scene.add(camera);
+  $container.append(renderer.domElement);
+  document.addEventListener( 'keydown', on_key_down, false );
 }
 
-for(var x = 0; x < CUBEGRID_ITERS; x++) {
-  for(var y = 0; y < CUBEGRID_ITERS; y++) {
-    var cube = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE);
-    cube.position.x = CUBEGRID_STEPSIZE * OFFSET_SCALE * x;
-    cube.position.y = CUBEGRID_STEPSIZE * OFFSET_SCALE * y;
-    cube.material.color.setRGB(0, COLOR_SCALE * x, COLOR_SCALE * y);
-    cubes.add(cube);
-  }
+function load_level() {
+  cube = create_cube(8, 8, 8, 1);
+  cube.position.set(0, 50, 0);
+  scene.add(cube);
+
+  var ground = create_cube(10, 1, 80, 0);
+  ground.position.set(0, -50, 0);
+  scene.add(ground);
+
+  // Add light to the scene
+  var pointlight = new THREE.PointLight( 0x999999, 2 );
+  pointlight.position.set( 50, 50, 500 );
+  scene.add(pointlight);
 }
 
-create_cube(80, 80, 80);
-scene.add(cubes);
-scene.add(camera);
-
-// Add light to the scene
-var pointlight = new THREE.PointLight( 0xFFFFFF );
-pointlight.position.x = 10;
-pointlight.position.y = 50;
-pointlight.position.z = 130;
-scene.add(pointlight);
 
 function render() {
   requestAnimationFrame(render);
 
+  scene.simulate();
+  //controls.update();
   renderer.render(scene, camera);
 }
 
+function on_key_down(event) {
+  switch(event.keyCode) {
+    case 37:
+      cube.applyCentralForce( new THREE.Vector3(0, 0, 80) );
+      break;
+    case 39:
+      cube.applyCentralForce( new THREE.Vector3(0, 0, -80) );
+      break;
+  }
+}
+
+function create_cube(width, height, length, mass) {
+  var cube_geom  = new THREE.CubeGeometry(width, height, length);
+  var cube_mat  = new THREE.MeshLambertMaterial({ color: 0xCC0000 });
+  var cube = new Physijs.BoxMesh(cube_geom, cube_mat, mass);
+  return cube;
+}
+
+init();
+load_level();
 render();
