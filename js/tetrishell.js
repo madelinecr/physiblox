28c8f862d9c3501a2d13f36ada956f72aa2b0cc3
Blaine Pace <blainepace@gmail.com>
2013-05-11 00:10:45 -0700

    Add the other 6 block shapes

--- js/tetrishell.js
+++ js/tetrishell.js
@@ -8,6 +8,13 @@ var VIEW_ANGLE = 45, ASPECT = WIDTH / HEIGHT, NEAR = 0.1, FAR = 10000;
 
 var CUBE_SIZE = 9, CUBE_MASS = 0.5;
 var MOVE_FORCE = 20;
+var I_PIECE_COLOR = 0x1ABC9C;
+var J_PIECE_COLOR = 0x2ECC71;
+var L_PIECE_COLOR = 0x3498DB;
+var O_PIECE_COLOR = 0x9B59B6;
+var S_PIECE_COLOR = 0xE74C3C;
+var T_PIECE_COLOR = 0xE67E22;
+var Z_PIECE_COLOR = 0x34495E;
 var $container = $('#container');
 var renderer, camera, scene, input_manager;
 var current_block;
@@ -70,12 +77,12 @@ function tick() {
     current_block.applyCentralForce( new THREE.Vector3(0, 0, -MOVE_FORCE) );
   } if(input_manager.rot_left == true) {
     console.log("rotate left");
-    var force_vector = new THREE.Vector3(0, 0, 0.2);
-    var offset_vector = new THREE.Vector3(0, 20, 0);
+    var force_vector = new THREE.Vector3(0, 0, 0.1);
+    var offset_vector = new THREE.Vector3(0, 15, 0);
     current_block.applyImpulse(force_vector, offset_vector);
   } if(input_manager.rot_right == true) {
-    var force_vector = new THREE.Vector3(0, 0, -0.2);
-    var offset_vector = new THREE.Vector3(0, 20, 0);
+    var force_vector = new THREE.Vector3(0, 0, -0.1);
+    var offset_vector = new THREE.Vector3(0, 15, 0);
     current_block.applyImpulse(force_vector, offset_vector);
   }
 }
@@ -131,18 +138,86 @@ function create_cube(width, height, length, color) {
   return cube;
 }
 
-function create_long() {
+function create_block(type) {
   var piece = create_cube(1, 1, 1, CUBE_MASS);
-  for(var i = 1; i <= 4; i++) {
-    var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, 0xCC00CC);
-    block.position.add(new THREE.Vector3(0, 0, (i-2.5) * CUBE_SIZE));
-    piece.add(block);
+  switch(type) {
+    case 1: // I piece
+      for(var i = 1; i <= 4; i++) {
+        var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, I_PIECE_COLOR);
+        block.position.add(new THREE.Vector3(0, 0, (i-2.5) * CUBE_SIZE));
+        piece.add(block);
+      }
+      return piece;
+    case 2: // J piece
+      for(var i = 1; i <= 3; i++) {
+        var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, J_PIECE_COLOR);
+        block.position.add(new THREE.Vector3(0, 0.5*CUBE_SIZE, (i-2) * CUBE_SIZE));
+        piece.add(block);
+      }
+      var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, J_PIECE_COLOR);
+      block.position.add(new THREE.Vector3(0, -0.5*CUBE_SIZE, -CUBE_SIZE));
+      piece.add(block);
+      return piece;
+    case 3: // L piece
+      for(var i = 1; i <= 3; i++) {
+        var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, L_PIECE_COLOR);
+        block.position.add(new THREE.Vector3(0, 0.5*CUBE_SIZE, (i-2) * CUBE_SIZE));
+        piece.add(block);
+      }
+      var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, L_PIECE_COLOR);
+      block.position.add(new THREE.Vector3(0, -0.5*CUBE_SIZE, CUBE_SIZE));
+      piece.add(block);
+      return piece;
+    case 4: // O piece
+      for(var x = -1; x <= 0; x++) {
+        for(var y = -1; y <= 0; y++) {
+          var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, O_PIECE_COLOR);
+          block.position.add(new THREE.Vector3(0, x*CUBE_SIZE+0.5*CUBE_SIZE, y*CUBE_SIZE+0.5*CUBE_SIZE));
+          piece.add(block);
+        }
+      }
+      return piece;
+    case 5: // S piece
+      for(var i = 1; i <= 2; i++) {
+        var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, S_PIECE_COLOR);
+        block.position.add(new THREE.Vector3(0, 0.5*CUBE_SIZE, (i-2) * CUBE_SIZE));
+        piece.add(block);
+      }
+      for(var i = 1; i <= 2; i++) {
+        var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, S_PIECE_COLOR);
+        block.position.add(new THREE.Vector3(0, -0.5*CUBE_SIZE, (i-1) * CUBE_SIZE));
+        piece.add(block);
+      }
+      return piece;
+    case 6: // T piece
+      for(var i = 1; i <= 3; i++) {
+        var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, T_PIECE_COLOR);
+        block.position.add(new THREE.Vector3(0, 0.5*CUBE_SIZE, (i-2) * CUBE_SIZE));
+        piece.add(block);
+      }
+      var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, T_PIECE_COLOR);
+      block.position.add(new THREE.Vector3(0, -0.5*CUBE_SIZE, 0));
+      piece.add(block);
+      return piece;
+    case 7: // Z piece
+      for(var i = 1; i <= 2; i++) {
+        var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, Z_PIECE_COLOR);
+        block.position.add(new THREE.Vector3(0, 0.5*CUBE_SIZE, (i-1) * CUBE_SIZE));
+        piece.add(block);
+      }
+      for(var i = 1; i <= 2; i++) {
+        var block = create_cube(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, Z_PIECE_COLOR);
+        block.position.add(new THREE.Vector3(0, -0.5*CUBE_SIZE, (i-2) * CUBE_SIZE));
+        piece.add(block);
+      }
+      return piece;
   }
-  return piece;
 }
 
 function get_block() {
-  var block = create_long();
+  // not the right way to generate random blocks
+  // TODO replace with grab-bag implementation
+  var block = create_block(Math.floor(Math.random() * 7 + 1));
   block.disabled = false;
   block.handle_collision = function(collided_with, linear_velocity, angular_velocity) {
     if(collided_with == left_wall) {
